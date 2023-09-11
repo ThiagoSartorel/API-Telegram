1210ef98581725f5d6b112f1861b2d6b4f059fb5
ThiagoSartorel <thiagosartorel@uniplac.net>
2023-09-11 10:40:06 -0300

    Working

---
+++ server.js
@@ -0,0 +1,49 @@
+const TeleBot = require("telebot");
+const express = require("express");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+// Instantiate Telebot with your Telegram token
+const bot = new TeleBot({
+  token: process.env.TOKEN,
+});
+
+// Create an Express.js web server
+const app = express();
+
+// Parse JSON requests
+app.use(bodyParser.json());
+
+// Define a route that listens for POST requests
+app.post("/send-message", async (req, res) => {
+  try {
+    const { message } = req.body;
+
+    if (!message) {
+      return res.status(500).json({
+        success: false,
+        message: "The attribute 'message' cannot be empty",
+      });
+    }
+
+    // Send a message to the specified chat ID
+    await bot.sendMessage(process.env.GROUP_ID, message);
+
+    // Respond to the HTTP request
+    res
+      .status(200)
+      .json({ success: true, message: "Success sending message!" });
+  } catch (error) {
+    console.error("Error sending message:", error);
+    res.status(500).json({ success: false, message: "Error sending message" });
+  }
+});
+
+// Start polling
+bot.start();
+
+// Start the Express server on port 3000 (or any other port you prefer)
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
